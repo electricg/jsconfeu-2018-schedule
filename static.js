6140d3bb16c436c4b12d11864db78c5c77f9dcab
Giulia Alfonsi <electric.g@gmail.com>
2018-05-24 15:44:40 -0700

    Static creation

---
+++ static.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use('/', express.static(`${__dirname}/out`));
+
+app.listen(port, err => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Ready on http://localhost:${port}`);
+});
